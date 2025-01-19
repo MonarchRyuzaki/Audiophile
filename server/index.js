a0aa5629a3dc042dbc114eb087d0636309ec5abc
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-01-19 13:13:16 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // Your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow cookies if needed
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/cart", cartRoutes);
